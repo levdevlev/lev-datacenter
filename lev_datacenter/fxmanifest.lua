44f4d42e07078a1ae97d2e7237e3128255e20c46
levdevlev <david.potisk1551@gmail.com>
2024-07-17 13:04:39 +0200

    Init

---
+++ lev_datacenter/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+this_is_a_map 'yes'
+
+files {
+    'audio/lev_glassdoor_game.dat151.rel',
+    'audio/lev_serverroom_interior_game.dat151.rel',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audio/lev_glassdoor_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/lev_serverroom_interior_game.dat'
